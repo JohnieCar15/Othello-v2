80a8b906111e22d9ce446c50053b55e7af45d98d
Johnie Carino <johniecarino15@gmail.com>
2023-05-01 15:33:04 +1000

    Modified gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/johnie/Othello-v2
-# Build directory: /home/johnie/Othello-v2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
