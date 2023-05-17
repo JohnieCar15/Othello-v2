2a180d3e873c080061a3491c9903c00e66a452ca
Johnie Carino <johniecarino15@gmail.com>
2023-05-17 23:55:00 +1000

    Implemented alpha beta function

--- src/ai.cpp
+++ src/ai.cpp
@@ -30,6 +30,57 @@ int Ai::minimax(int depth, int nodeIndex, bool isMax, int scores[], int h) {
             minimax(depth+1, nodeIndex*2 + 1, true, scores, h));
 }
 
+// Returns optimal value for
+// current player(Initially called
+// for root and maximizer)
+int Ai::alphaBeta(int depth, int nodeIndex, bool maximizingPlayer, int values[], int alpha, int beta) {
+     
+    // Terminating condition. i.e
+    // leaf node is reached
+    if (depth == 3)
+        return values[nodeIndex];
+ 
+    if (maximizingPlayer)
+    {
+        int best = MIN;
+ 
+        // Recur for left and
+        // right children
+        for (int i = 0; i < 2; i++)
+        {
+             
+            int val = alphaBeta(depth + 1, nodeIndex * 2 + i,
+                              false, values, alpha, beta);
+            best = std::max(best, val);
+            alpha = std::max(alpha, best);
+ 
+            // Alpha Beta Pruning
+            if (beta <= alpha)
+                break;
+        }
+        return best;
+    }
+    else
+    {
+        int best = MAX;
+ 
+        // Recur for left and
+        // right children
+        for (int i = 0; i < 2; i++)
+        {
+            int val = alphaBeta(depth + 1, nodeIndex * 2 + i,
+                              true, values, alpha, beta);
+            best = std::min(best, val);
+            beta = std::min(beta, best);
+ 
+            // Alpha Beta Pruning
+            if (beta <= alpha)
+                break;
+        }
+        return best;
+    }
+}
+
 
 
 float Ai::coinParity(Piece (&grid)[8][8], Piece p) {
--- src/ai.h
+++ src/ai.h
@@ -9,6 +9,9 @@
 
 #include "piece.h"
 
+const int MAX = 1000;
+const int MIN = -1000;
+
 class Ai {
 public:
     Ai();
@@ -16,6 +19,7 @@ public:
 
     std::pair<int, int> randomMove(std::vector<std::pair<int, int>> &moves);
     int minimax(int depth, int nodeIndex, bool isMax, int scores[], int h);
+    int alphaBeta(int depth, int nodeIndex, bool maximizingPlayer, int values[], int alpha, int beta);
 
     /* Captures difference in coins between max player and min player */
     float coinParity(Piece (&grid)[8][8], Piece p);
