365b3ac32255b15755543342a81a9bb547e99170
Johnie Carino <johniecarino15@gmail.com>
2023-06-06 23:29:37 +1000

    Inserted pseudo code for alpha beta pruning algorithm

--- src/ai.cpp
+++ src/ai.cpp
@@ -13,6 +13,35 @@ std::pair<int, int> Ai::randomMove(std::vector<std::pair<int, int>> moves) {
 // current player(Initially called
 // for root and maximizer)
 int Ai::alphaBeta(Piece **board, int depth, bool maximizingPlayer, int alpha, int beta) {
+    /*
+    PSEUDO CODE
+
+    function minimax(node, depth, alpha, beta, maximizingPlayer) is  
+    if depth ==0 or node is a terminal node then  
+    return static evaluation of node  
+    
+    if MaximizingPlayer then      // for Maximizer Player  
+    maxEva= -infinity            
+    for each child of node do  
+        eva= minimax(child, depth-1, alpha, beta, False)  
+        maxEva= max(maxEva, eva)   
+        alpha= max(alpha, maxEva)      
+        if beta<=alpha  
+            break  
+    return maxEva  
+        
+    else                         // for Minimizer player  
+    minEva= +infinity   
+    for each child of node do  
+        eva= minimax(child, depth-1, alpha, beta, true)  
+        minEva= min(minEva, eva)   
+        beta= min(beta, eva)  
+        if beta<=alpha  
+            break          
+    return minEva  
+    
+    */
+    
     (void) board;
     (void) depth;
     (void) maximizingPlayer;
